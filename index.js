fa987c2e63fef8be0db072965d185a0d6c00924e
Ajit Gupta <57554561+devajitgupta@users.noreply.github.com>
2020-02-10 23:39:21 -0800

    Add files via upload

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const app = express();
+const path = require('path')
 app.get("/",function(req,res){
 
 });
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3020;
 
 app.listen(port, function(req,res){
-	console.log("hello world program running");
+	console.log("hello world program running" , port);
 });
